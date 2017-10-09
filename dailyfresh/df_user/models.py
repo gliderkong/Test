89ef07eb1adcd6c454c1aa80a9e8c98891798be9
gliderkong <2584734488@qq.com>
2017-10-09 21:57:45 +0800

    number7

--- dailyfresh/df_user/models.py
+++ dailyfresh/df_user/models.py
@@ -3,6 +3,31 @@ from db.base_model import BaseModel   #  导入抽象模型基类
 
 
 # Create your models here.
+class PassportManager(models.Manager):
+    '''
+    用户账户模型管理器类
+    '''
+    def add_one_passport(self,username,password,email):
+        '''
+        添加一个用户注册信息
+        '''
+        models_class = self.model
+
+        # 创建模型类对象
+        passport = models_class()
+        passport = Passport()
+        # 创建了一个 Passport()的对象,名字为 passport
+        passport.username = username
+        # 对象 passport的参数passport.username 为 POST请求接受的 username
+        passport.password = password
+        passport.email = email
+        # 使用 save()保存到数据库当中
+        passport.save()
+
+        # 返回一个passport 对象
+        return passport
+
+
 
 class Passport(BaseModel):
     '''
@@ -11,6 +36,10 @@ class Passport(BaseModel):
     username = models.CharField(max_length=20, verbose_name='用户名')
     password = models.CharField(max_length=40, verbose_name='密码')
     email = models.EmailField (verbose_name="邮箱")
+
+
+    # 在类当中创建了一个对象,对象的类方法在上面
+    objects = PassportManager()  # 自定义模型管理器对象
 
     class Meta:
             db_table = "s_user_account"     # 一般不适用默认的表名,所以创建一个自定义表名
--- dailyfresh/df_user/views.py
+++ dailyfresh/df_user/views.py
@@ -1,43 +1,67 @@
 from django.shortcuts import render,redirect
+
+
+from django.views.decorators.http import require_POST,require_GET,require_http_methods
 from django .template import loader
 from df_user.models import Passport
+
+
+
+#----------------------------------------------------------------
+# 在注册页面中表单请求方式进行限定,创建一个装饰器,在不影响函数的情况下添加新的功能
+# 原生 限制非法请求通道装饰器方法
+'''
+def request_POST (view_func):
+    def wrapper(request,*args,**kwargs):
+        if request.method != 'POST':
+            # 如果不是post请求则拒绝提交,同时返回一个提示信息
+            return HttpResponseNotAllowed('not allowed')
+        else :
+            return view_func(request, *args, **kwargs)
+    return  wrapper
+'''
 
 # Create your views here.
 # http: //127.0.0.1:8000 /user/register/
 
+# Django 封装的装饰器方法
+@require_http_methods(['GET','POST'])
+
 def register(request):
     '''
-    显示注册页面
+    显示注册页面 / 用户注册登陆合二为一  添加到一个 视图函数当中
     '''
-    return render (request, 'df_user/register.html')
+    # 视图函数只负责接受发送来的请求,无论是什么方式提交,只要符合即会返回指定页面
+    # 通过网址登陆网页,全部向服务器发送 get 请求, 所以可以 设置为 get 参数
+    # 注册表单向服务器发送为post请求方式,所以可以设置为 只有POST请求方式才可以跳转
+
+    if request.method == 'GET':
+        return render (request,'df_user/register.html')
+
+    elif request.method == "POST":
+        '''
+        用户注册处理
+
+        '''
+        # 用户在注册页面中 提交表单信息之后,发送到当前的视图函数中
+        # 创建一个视图函数,用于接受用户的注册信息
+
+        # 1 接受用户提交的信息
+        username = request.POST.get('user_name')
+        password = request.POST.get('pwd')
+        email = request.POST.get('email')
+
+        # 2 将用户的注册信息保存到数据库
 
 
-#----------------------------------------------------------------
-# 用户在注册页面中 提交表单信息之后,发送到当前的视图函数中
-# 创建一个视图函数,用于接受用户的注册信息
+        # 3 跳转到登陆页面
+        return redirect('/')
 
-def register_handle(request):
-    '''
-    用户注册处理
-    '''
-    # 1 接受用户提交的信息
-    username = request.POST.get('user_name')
-    password = request.POST.get('pwd')
-    email = request.POST.get('email')
-
-    # 2 讲用户的注册信息保存到数据库
-    passport = Passport()
-    # 创建了一个 Passport()的对象,名字为 passport
-    passport.username = username
-    # 对象 passport的参数passport.username 为 POST请求接受的 username
-    passport.password = passport
-    # 同理
-    passport.email = email
-    # 同理
-    passport.save()
+        # return "OK"
 
 
-    # 3 跳转到登陆页面
-    return redirect('/')
+
+
+
 
 
